c512379b206d82cb1fcc645123a5fa7ceecff000
Dave Woods <dwoodscs@gmail.com>
2020-03-06 22:09:40 +0000

    Updated sparkline to use bars, changed date display to be more readable

--- src/views/Sessions.vue
+++ src/views/Sessions.vue
@@ -1,29 +1,45 @@
 <template>
 	<v-container>
-		<v-card elevation="0" class="mb-6" style="position: relative;">
+		<v-card elevation="0" class="mb-6" style="position: relative" color="#134654" dark>
 			<v-card-title>Sessions</v-card-title>
 			<v-card-subtitle>
-				Listing {{ sessionList.length }} squad sessions from {{ sessionList[0].date }} to {{ sessionList[sessionList.length-1].date }}
+				Listing {{ sessionList.length }} squad sessions from {{ fromDate }} to {{ toDate }}
 			</v-card-subtitle>
 			<v-card-text>
 				<v-sparkline
 					:value="attendanceRates"
+					color="white"
 					auto-draw
-					smooth="2"
-					stroke-linecap="round"
+					:smooth="1"
+					:line-width="10"
+					class="py-6"
+					type="bar"
 				></v-sparkline>
 			</v-card-text>
 			<v-dialog max-width="800px" v-model="displayForm" @input="v => v || this.$refs.nsform.resetForm()">
 				<template v-slot:activator="{ on }">
-					<v-btn v-on="on" color="#66A074" absolute right bottom fab title="Add new session"><v-icon>mdi-plus</v-icon></v-btn>
+					<v-btn
+						v-on="on"
+						light
+						absolute
+						right
+						bottom
+						fab
+						title="Add new session"
+					>
+						<v-icon
+							color="#134654"
+							large
+						>mdi-plus</v-icon>
+					</v-btn>
 				</template>
-				<new-session-form ref="nsform" @save-form="displayForm = false"></new-session-form>
+				<new-session-form ref="nsform" @close-form="displayForm = false"></new-session-form>
 			</v-dialog>
 		</v-card>
 		<v-timeline>
 			<v-timeline-item fill-dot v-for="session in reversedSessionList" :key="session.date" color="#3C7364">
 				<template v-slot:opposite>
-					<span class="headline font-weight-bold">{{ session.date }}</span>
+					<span class="headline font-weight-bold">{{ dateToDateString(session.date) }}</span>
 				</template>
 				<v-card>
 					<v-card-title>"{{ session.subtitle || 'Squad' }}"</v-card-title>
@@ -61,12 +77,25 @@ export default {
 		},
 		attendanceRates() {
 			return this.sessionList.map(s => s.tof.length)
+		},
+		fromDate() {
+			return this.dateToDateString(this.sessionList[0].date)
+		},
+		toDate() {
+			return this.dateToDateString(this.sessionList[this.sessionList.length - 1].date)
 		}
 	},
 	methods: {
 		getMember(id) {
 			return this.$store.getters.getMemberById(id)
 		},
+		dateToDateString(d) {
+			return new Date(d).toLocaleDateString('en-IE', {
+				day: 'numeric',
+				month: 'long',
+				year: 'numeric'
+			})
+		}
 	},
 	components: {
 		'new-session-form': NewSessionForm
